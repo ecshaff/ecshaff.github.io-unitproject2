91c0254542569c664d9d547cf5aa306b66c6c29f
ecshaff <emmyshaff@gmail.com>
2019-05-03 02:09:28 -0700

    index page added

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 require('dotenv').config()
 const app = express()
-
+let Store = require('./models/store.js')
 
 const PORT = process.env.PORT
 const mongoURI = process.env.MONGODB_URI
@@ -14,6 +14,30 @@ app.use(express.urlencoded({ extended: false }))
 app.get('/new', function(req, res){
 	res.render('./store/new.ejs')
 })
+
+app.get('/', function(req, res) {
+	Store.find({}, function(error, store){
+		if (error){ console.log(error) }
+	res.render('./store/index.ejs', {store})
+})
+	})
+
+app.post ( '/' , function(req, res) {
+  Store.create( req.body , function(error, store) {
+    if (error) { res.send (error) ; } else {
+      res.redirect( '/store/'+ store.id );
+    }
+  });
+});
+
+
+app.put( '/:id' , function(req, res) {
+  Store.findByIdAndUpdate( req.params.id, req.body , { new : true }, function(error, product)  {
+    if (error) { console.log(error); }
+    res.redirect ( '/store/' + store.id );
+  });
+});
+
 
 mongoose.connect(mongoURI, { useNewUrlParser: true })
 mongoose.connection.once('open', () => {
