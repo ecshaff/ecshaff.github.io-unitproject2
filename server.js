7887f65236687a39bd4ba644f9acb88065f9d4b0
ecshaff <emmyshaff@gmail.com>
2019-05-07 21:43:28 -0700

    make it

--- server.js
+++ server.js
@@ -164,5 +164,7 @@ app.post('/store/', function(req, res){
 
 
 
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
-app.listen(process.env.PORT || PORT);
